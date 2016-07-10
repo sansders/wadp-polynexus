2f5a229deda49a911497e5d3011553fa56a3340f
Jing Zhi <t.jingzhi1@gmail.com>
2016-07-11 04:17:40 +0800

    added themes
    and french language
    and started on mobile webpage

--- js/login.js
+++ js/login.js
@@ -7,15 +7,18 @@ function login(){
   var input = document.getElementById('usernameinput').value;
   input = input.toLowerCase();
   try{
+    //load session storage
     var username = eval("xmlDoc.users['"+input+"'].username");
     var language = eval("xmlDoc.users['"+input+"'].lang");
     var groupchats = eval("xmlDoc.users['"+input+"'].groupchats");
     var privatechats = eval("xmlDoc.users['"+input+"'].privatechats");
+    var theme = eval("xmlDoc.users['"+input+"'].theme");
     sessionStorage.setItem('username' , username);
     sessionStorage.setItem('language' , language);
     sessionStorage.setItem('userid', input);
     sessionStorage.setItem('groupchats', groupchats);
     sessionStorage.setItem('privatechats', privatechats);
+    sessionStorage.setItem('theme', theme);
     document.location = "main.html"
   }
   catch(err){
