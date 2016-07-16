164cae850cd3ba39b1da494c323172b3e48b3ea9
Grymbel <grymb3l@gmail.com>
2016-07-17 00:49:08 +0800

    FAQ has theme changeable, BGs elongated

--- js/rulesFAQ.js
+++ js/rulesFAQ.js
@@ -2,19 +2,19 @@ function changeTheTheme(n){
   if(n === 'mono'){
     //monochrome theme
     var sheet = document.createElement('style');
-    sheet.innerHTML ="h1 {background-color:rgba(180,180,180,0.7)}body {background-image: url(img/3mono.jpg);} .intro h2{color:rgba(255,255,255,1);} .intro p{color:rgba(255,255,255,1)}";
+    sheet.innerHTML ="button {background-color:rgba(180,180,180,1);} button:hover{background-color: rgba(180,180,180,0.7);} h1 {background-color:rgba(180,180,180,0.7)}body {background-image: url(img/3mono.jpg);} .intro h2{color:rgba(255,255,255,1);} .intro p{color:rgba(255,255,255,1)}";
     document.body.appendChild(sheet);
     console.log("Theme changed to mono");
   }
   else if(n === 'pink'){
     var sheet = document.createElement('style');
-    sheet.innerHTML = "h1 {background-color: rgba(255,255,255,0.7); color:black;} body {background-image: url(img/3pink.jpg);} .intro h2{color:rgba(0,0,0,0.8);} .intro p{color:rgba(0,0,0,0.8)}";
+    sheet.innerHTML = "button {background-color:rgba(255,105,180,1);} button:hover{background-color: rgba(255,150,180,0.4);} h1 {background-color: rgba(255,255,255,0.7); color:black;} body {background-image: url(img/3pink.jpg);} .intro h2{color:rgba(0,0,0,0.8);} .intro p{color:rgba(0,0,0,0.8)}";
     document.body.appendChild(sheet);
     console.log("Theme changed to pink");
   }
   else if(n === 'classic'){
     var sheet = document.createElement('style');
-    sheet.innerHTML = "h1 {background-color: rgba(0, 76, 159, 0.7);} body {background-image: url(img/3classic.jpg);} .intro h2{color:rgba(255,255,255,1);} .intro p{color:rgba(255,255,255,1)}";
+    sheet.innerHTML = "button{background-color: rgba(0, 76, 159, 0.7);} button:hover{background-color: rgba(0, 76, 159, 0.4);} h1 {background-color: rgba(0, 76, 159, 0.7);} body {background-image: url(img/3classic.jpg);} .intro h2{color:rgba(255,255,255,1);} .intro p{color:rgba(255,255,255,1)}";
     document.body.appendChild(sheet);
     console.log("Theme changed to classic");
   }
