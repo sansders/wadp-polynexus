1cbf5ad1f5a015c1acd2688e47b95289f6617277
Grymbel <grymb3l@gmail.com>
2016-07-17 20:11:34 +0800

    Last try for today to fix themes

--- js/rulesFAQ.js
+++ js/rulesFAQ.js
@@ -5,6 +5,7 @@ function changeTheTheme(n){
     console.log("Attempting to change theme to mono");
     sheet.innerHTML ="button {background-color:rgba(180,180,180,1);} button:hover{background-color: rgba(180,180,180,0.7);} h1 {background-color:rgba(180,180,180,0.7)}body {background-image: url(img/3mono.jpg);} .intro h2{color:rgba(255,255,255,1);} .intro p{color:rgba(255,255,255,1)}";
     document.body.appendChild(sheet);
+    sessionStorage.setItem("theme","mono");
     console.log("Theme changed to mono");
   }
   else if(n === 'pink'){
@@ -12,6 +13,7 @@ function changeTheTheme(n){
     console.log("Attempting to change theme to pink");
     sheet.innerHTML = "button {background-color:rgba(255,105,180,1);} button:hover{background-color: rgba(255,150,180,0.4);} h1 {background-color: rgba(255,255,255,0.7); color:black;} body {background-image: url(img/3pink.jpg);} .intro h2{color:rgba(0,0,0,0.8);} .intro p{color:rgba(0,0,0,0.8)}";
     document.body.appendChild(sheet);
+    sessionStorage.setItem("theme","pink");
     console.log("Theme changed to pink");
   }
 
@@ -20,6 +22,7 @@ function changeTheTheme(n){
     console.log("Attempting to change theme to classic");
     sheet.innerHTML = "button{background-color: rgba(0, 76, 159, 0.7);} button:hover{background-color: rgba(0, 76, 159, 0.4);} h1 {background-color: rgba(0, 76, 159, 0.7);} body {background-image: url(img/3classic.jpg);} .intro h2{color:rgba(255,255,255,1);} .intro p{color:rgba(255,255,255,1)}";
     document.body.appendChild(sheet);
+    sessionStorage.setItem("theme","classic");
     console.log("Theme changed to classic");
   }
   else {
