1e1ebf855c31d4dbb45bdbc29ee10b8ff91f2b37
Grymbel <grymb3l@gmail.com>
2016-07-17 19:59:50 +0800

    Reverting changes

--- js/rulesFAQ.js
+++ js/rulesFAQ.js
@@ -8,16 +8,13 @@ function changeTheTheme(n){
     console.log("Theme changed to mono");
   }
   else if(n === 'pink'){
-    console.log("Attempting to change theme to pink")
-    $("button").css("background-color", "rgba(255,105,180,1);");
-    $("button:hover").css("background-color", "rgba(255,150,180,0.4);");
-    $("h1").css("background-color", "rgba(255,255,255,0.7)");
-    $("h1").css("color", "black");
-    $("body").css("background-image", "url(img/3pink.jpg)");
-    $(".intro h2").css("color","rgba(0,0,0,0.8)");
-    $(".intro p").css("color","rgba(0,0,0,0.8)");
-    console.log("Finished changing theme to pink")
+    var sheet = document.createElement('style');
+    console.log("Attempting to change theme to pink");
+    sheet.innerHTML = "button {background-color:rgba(255,105,180,1);} button:hover{background-color: rgba(255,150,180,0.4);} h1 {background-color: rgba(255,255,255,0.7); color:black;} body {background-image: url(img/3pink.jpg);} .intro h2{color:rgba(0,0,0,0.8);} .intro p{color:rgba(0,0,0,0.8)}";
+    document.body.appendChild(sheet);
+    console.log("Theme changed to pink");
   }
+
   else if(n === 'classic'){
     var sheet = document.createElement('style');
     console.log("Attempting to change theme to classic");
