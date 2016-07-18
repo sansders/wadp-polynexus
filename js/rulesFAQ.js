42b09fff08457deb19bb6c91816398a94419d253
SengokuMedaru <sean_tay1999@hotmail.com>
2016-07-18 12:51:40 +0800

    Applying fix for themes

--- js/rulesFAQ.js
+++ js/rulesFAQ.js
@@ -4,16 +4,16 @@ function changeTheTheme(n){
     var sheet = document.createElement('style');
     console.log("Attempting to change theme to mono");
     sheet.innerHTML ="button {background-color:rgba(180,180,180,1);} button:hover{background-color: rgba(180,180,180,0.7);} h1 {background-color:rgba(180,180,180,0.7)}body {background-image: url(img/3mono.jpg);} .intro h2{color:rgba(255,255,255,1);} .intro p{color:rgba(255,255,255,1)}";
-    document.body.appendChild(sheet);
     sessionStorage.setItem("theme","mono");
+	document.body.appendChild(sheet);
     console.log("Theme changed to mono");
   }
   else if(n === 'pink'){
     var sheet = document.createElement('style');
     console.log("Attempting to change theme to pink");
+	sessionStorage.setItem("theme","pink");
     sheet.innerHTML = "button {background-color:rgba(255,105,180,1);} button:hover{background-color: rgba(255,150,180,0.4);} h1 {background-color: rgba(255,255,255,0.7); color:black;} body {background-image: url(img/3pink.jpg);} .intro h2{color:rgba(0,0,0,0.8);} .intro p{color:rgba(0,0,0,0.8)}";
     document.body.appendChild(sheet);
-    sessionStorage.setItem("theme","pink");
     console.log("Theme changed to pink");
   }
 
