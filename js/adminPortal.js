36fc9f864ca25f2338f00fe0afc6da425c7b0fe9
SengokuMedaru <sean_tay1999@hotmail.com>
2016-07-28 23:29:13 +0800

    Added user list into adminPortal
    Yes, it's not perfect as it has to be manually updated.... for now

--- js/adminPortal.js
+++ js/adminPortal.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 	if(user === null){
 		window.location = 'adminlogin.html';
 	}
-	else if(perm == 0){
+	else if(perm === 0){
+		alert("Higher permission required!");
 		window.location = 'main.html';
-		alert("Higher permission required!")
 	}
 });
 
@@ -19,8 +19,7 @@ function logout(){
   sessionStorage.removeItem('groupchats');
   sessionStorage.removeItem('privatechats');
   sessionStorage.removeItem('perm');
-}
-});
+};
 $(document).ready(function(){
 	$("#h1-left").click(function(){
 		$("#sidenav").toggle("drop");
@@ -34,19 +33,71 @@ function addToList(wordToAdd){
 	document.getElementById('wordList').appendChild(newWord);
 }
 
-function listUsernames(){
-	/* Creating new li to store data in. 'A' variables will also be created below. */
-	var liX = document.createElement('li');
-	var bigDiv = document.getElementById('usernames');
-	var pX = document.createElement('p');
-
-	var userusername = //attempt to extract data from json data file
-	var textnode0 = document.createTextNode(userusername);
-
-	pX.appendChild(textnode0);
-
-
-	liX.appendChild(pX);
-	bigDiv.appendChild(liX);
-
+function readTextFile(file)
+{
+	var rawFile = new XMLHttpRequest();
+    rawFile.open("GET","../data/data.json", false);
+	console.log('running1');
+	alert(allText);
+	console.log('running2');
+    rawFile.send(null); 	
+	
 }
+
+
+$(document).ready(function listUsernames(){
+	
+	console.log('running');
+	
+	var ulX = document.getElementById('usernames');
+	var liX0 = document.createElement('li');
+	var liX1 = document.createElement('li');
+	var liX2 = document.createElement('li');
+	var liX3 = document.createElement('li');
+	var liX4 = document.createElement('li');
+	var liX5 = document.createElement('li');
+	
+	
+	console.log('running2');
+
+	var userusername0 = "161714f";
+	var userusername1 = "123456a";
+	var userusername2 = "111111a";
+	var userusername3 = "160280x";
+	var userusername4 = "160364n";
+	var userusername5 = "bbbelson";
+
+	
+	
+	//attempt to extract data from json data file soon!!!
+	
+	
+	
+	var textnode0 = document.createTextNode(userusername0);
+	var textnode1 = document.createTextNode(userusername1);
+	var textnode2 = document.createTextNode(userusername2);
+	var textnode3 = document.createTextNode(userusername3);
+	var textnode4 = document.createTextNode(userusername4);
+	var textnode5 = document.createTextNode(userusername5);
+
+	console.log('running3');
+	
+	liX0.appendChild(textnode0);
+	liX1.appendChild(textnode1);
+	liX2.appendChild(textnode2);
+	liX3.appendChild(textnode3);
+	liX4.appendChild(textnode4);
+	liX5.appendChild(textnode5);
+
+	
+	
+	console.log('running4');
+
+	ulX.appendChild(liX0);
+	ulX.appendChild(liX1);
+	ulX.appendChild(liX2);
+	ulX.appendChild(liX3);
+	ulX.appendChild(liX4);
+	ulX.appendChild(liX5);
+
+});
