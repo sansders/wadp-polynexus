34a06bf242055b124492fdbff8305352e78ab3c2
Grymbel <grymb3l@gmail.com>
2016-08-01 17:29:54 +0800

    Added a few new files, will work later

--- js/icons.js
+++ js/icons.js
@@ -10,49 +10,49 @@ function setIcon(iconNo){
     case 'icon0':
     document.getElementById("changeableIcon").src="icons/icon0.jpg";
     document.getElementById("changeableIcon1").src="icons/icon0.jpg";
-    sessionStorage.setItem("icon","0");
+    sessionStorage.setItem("icon","icon0");
     break;
 
     case 'icon1':
     document.getElementById("changeableIcon").src="icons/icon1.jpg";
     document.getElementById("changeableIcon1").src="icons/icon1.jpg";
-    sessionStorage.setItem("icon","1");
+    sessionStorage.setItem("icon","icon1");
     break;
 
     case 'icon2':
     document.getElementById("changeableIcon").src="icons/icon2.jpg";
     document.getElementById("changeableIcon1").src="icons/icon2.jpg";
-    sessionStorage.setItem("icon","2");
+    sessionStorage.setItem("icon","icon2");
     break;
 
     case 'icon3':
     document.getElementById("changeableIcon").src="icons/icon3.jpg";
     document.getElementById("changeableIcon1").src="icons/icon3.jpg";
-    sessionStorage.setItem("icon","3");
+    sessionStorage.setItem("icon","icon3");
     break;
 
     case 'icon4':
     document.getElementById("changeableIcon").src="icons/icon4.jpg";
     document.getElementById("changeableIcon1").src="icons/icon4.jpg";
-    sessionStorage.setItem("icon","4");
+    sessionStorage.setItem("icon","icon4");
     break;
 
     case 'icon5':
     document.getElementById("changeableIcon").src="icons/icon5.jpg";
     document.getElementById("changeableIcon1").src="icons/icon5.jpg";
-    sessionStorage.setItem("icon","5");
+    sessionStorage.setItem("icon","icon5");
     break;
 
     case 'icon6':
     document.getElementById("changeableIcon").src="icons/icon6.jpg";
     document.getElementById("changeableIcon1").src="icons/icon6.jpg";
-    sessionStorage.setItem("icon","6");
+    sessionStorage.setItem("icon","icon6");
     break;
 
     case 'icon7':
     document.getElementById("changeableIcon").src="icons/icon7.jpg";
     document.getElementById("changeableIcon1").src="icons/icon7.jpg";
-    sessionStorage.setItem("icon","7");
+    sessionStorage.setItem("icon","icon7");
     break;
 
     default: console.log(iconNo);
